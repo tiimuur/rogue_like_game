efba6028dc3abc4e682d5247c0912de41016d3d2
tiimuur <timkawert.ru@gmail.com>
2023-11-13 21:43:34 +0300

    bullet destroying

--- Scenes/bullet.gd
+++ Scenes/bullet.gd
@@ -18,3 +18,9 @@ func _physics_process(delta):
 
 func _on_visible_on_screen_enabler_2d_screen_exited():
 	queue_free()
+
+
+
+func _on_body_entered(body):
+	if body.name != "Player":
+		queue_free()
