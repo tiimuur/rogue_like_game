ca820901a701e05cc88ea9b8c41082d8c35d5fe2
tiimuur <98406636+tiimuur@users.noreply.github.com>
2024-05-17 21:16:23 +0300

    main_menu_fix

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _on_play_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Level1/Tilemap.tscn")
+	get_tree().change_scene_to_file("res://firstTryToGetRandom/funny_coridor.tscn")
 
 
 func _on_quit_button_pressed():
