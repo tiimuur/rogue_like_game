f9b7fc58b25fc4e854ae127d5198c32e9c6ab157
ARSENIQ <merarseniebars99@gmail.com>
2024-05-23 14:50:13 +0300

    some fixes
    del double code

--- Mobs/Enemy.gd
+++ Mobs/Enemy.gd
@@ -19,24 +19,24 @@ func _physics_process(delta):
 			player.getDamaged(50)
 			$EnemyTimer.start(1)
 			get_parent().update_hp()
-			
+		if not canDamage:
+			$AnimatedSprite2D.play("attack")
 		elif chase and player.alive:
 			navigation_agent.target_position = player.global_position
-			if !(navigation_agent.is_target_reached()):
+			
+			if !(navigation_agent.is_target_reached()) and navigation_agent.is_target_reachable():
 				var direction = (navigation_agent.get_next_path_position() - self.global_position).normalized()
 				velocity = direction * speed
 				move_and_collide(velocity)
-			$AnimatedSprite2D.play("walk")
-			
+				$AnimatedSprite2D.play("walk")
 		else:
-			velocity = Vector2()
+			$AnimatedSprite2D.play("idle")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if alive:
 		velocity = Vector2()
 		var player = get_parent().get_player()
-		#var direction = (player.position - self.position).normalized()
 		if playerNextTo and player.alive:
 			if canDamage:
 				canDamage = false
@@ -47,12 +47,10 @@ func _process(delta):
 		if not canDamage:
 			$AnimatedSprite2D.play("attack")
 		elif chase and player.alive:
-			navigation_agent.target_position = Vector2(16, 16)
 			$AnimatedSprite2D.play("walk")
 		else:
 			velocity = Vector2()
 			$AnimatedSprite2D.play("idle")
-		#move_and_collide(velocity)
 	
 	
 func _on_area_2d_body_entered(body):
